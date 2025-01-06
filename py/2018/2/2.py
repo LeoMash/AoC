5bd558fe3b9cf1f396819ec4bda87f4c52349827
Leonid Mashinskiy <LeoMash@gmail.com>
2025-01-06 23:28:33 +0300

    Solutions for Days 1-2 from 2018

---
+++ py/2018/2/2.py
@@ -0,0 +1,18 @@
+f = '2.in'
+D = open(f).read().strip()
+R = [int(line) for line in D.splitlines()]
+
+ans1 = sum(R)
+print(ans1)
+
+ans2 = 0
+vis = set()
+i = 0
+while True:
+    r = R[i % len(R)]
+    ans2 += r
+    if ans2 in vis:
+        break
+    vis.add(ans2)
+    i += 1
+print(ans2)
